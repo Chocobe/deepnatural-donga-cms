07bbd88f6ca98618884e88a43fcd2278ced1a63d
Miles <yw@deepnatural.ai>
2024-07-29 10:37:20 +0900

    Create FindPasswordPage

--- src/pages/auth/FindPasswordPage/FindPasswordPage.tsx
+++ src/pages/auth/FindPasswordPage/FindPasswordPage.tsx
@@ -1,29 +1,74 @@
 // FIXME: mockup page
 
-import { cn } from '@/lib/shadcn-ui-utils';
-import { NavLink } from 'react-router-dom';
+// react
+import {
+  useCallback,
+} from 'react';
+// router
+import { useNavigate, NavLink } from 'react-router-dom';
 import routePathFactory from '@/routes/routePathFactory';
+// ui
+import { Button } from '@/components/shadcn-ui/ui/button';
+// style
+import './FindPasswordPage.css';
 
 function FindPasswordPage() {
-  return (
-    <div className={cn(
-      'w-full h-full',
-      'flex flex-col justify-center items-center gap-5',
-    )}>
-      <div className="text-center text-lg">
-        Find Password Page
-      </div>
+  //
+  // hook
+  //
+  const navigate = useNavigate();
 
-      <NavLink 
-        to={routePathFactory.auth.getLoginPagePath()}
-        style={{
-          color: '#6366f1',
-          fontSize: '14px',
-          lineHeight: '22px',
-          fontWeight: '400',
-        }}>
-        로그인 페이지
-      </NavLink>
+  const findPassword = useCallback(() => {
+    console.log('findPassword()');
+
+    navigate(routePathFactory.auth.getLoginPagePath());
+  }, [navigate]);
+
+  return (
+    <div className="LoginPage">
+      <div className="form">
+        {/* formHeader */}
+        <div className="formHeader">
+          <figure className="logoWrapper">
+            <img
+              className="logo"
+              src="images/donga-logo.png"
+              alt="동아출판"
+            />
+          </figure>
+
+          <div className="titleWrapper">
+            <h1 className="cmsTitle">
+              동아출판 CMS
+            </h1>
+            <h2 className="loginTitle">
+              로그인
+            </h2>
+          </div>
+        </div>
+
+        {/* formBody */}
+        <div className="formBody">
+          <div className="findPassword-mockup">
+            TBU
+          </div>
+
+          <Button
+            className="findPasswordButton"
+            onClick={findPassword}>
+            비밀번호 찾기
+          </Button>
+        </div>
+
+        {/* formFooter */}
+        <div className="formFooter">
+          <NavLink
+            className="findPasswordPageButton"
+            to={routePathFactory.auth.getLoginPagePath()}>
+            로그인
+          </NavLink>
+        </div>
+      </div>
     </div>
   );
 }
