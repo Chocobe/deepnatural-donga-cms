e40cca009dcaa08b7dd085bb0767efcecc0fdf77
Miles <yw@deepnatural.ai>
2024-07-30 17:45:45 +0900

    Apply BASE_URL to api

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default (function createAPI() {
   const api = axios.create({
-    baseURL: 'https://jsonplaceholder.typicode.com',
+    baseURL: import.meta.env.VITE_API_BASE_URL,
     timeout: 10_000,
   });
 
