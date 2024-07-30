8375c75a47e38c32fbb486dfa2f376b6a0ceba85
Miles <yw@deepnatural.ai>
2024-07-30 18:15:59 +0900

    Apply login api to LoginPage

--- src/pages/auth/LoginPage/LoginPage.tsx
+++ src/pages/auth/LoginPage/LoginPage.tsx
@@ -1,16 +1,19 @@
 // react
 import {
   useState,
-  useMemo,
-  useCallback,
   ChangeEvent,
+  FormEvent,
 } from 'react';
 // router
 import { 
   NavLink,
-  useNavigate,
 } from 'react-router-dom';
 import routePathFactory from '@/routes/routePathFactory';
+// store
+import useAuthStore from '@/store/authStore/authStore';
+import { 
+  createSuccessApiState,
+} from '@/store/apiStateUtils';
 // ui
 import { 
   Input,
@@ -18,6 +21,12 @@ import {
 import { 
   Button,
 } from '@/components/shadcn-ui/ui/button';
+// api
+import ApiManager from '@/apis/ApiManager';
+// type
+import { 
+  TLoginPayload,
+} from '@/apis/auth/auth.type';
 // style
 import './LoginPage.css';
 
@@ -25,59 +34,65 @@ function LoginPage() {
   //
   // state
   //
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [formState, setFormState] = useState({
+    username: '',
+    password: '',
+  });
+
+  const formTemplates = [
+    {
+      id: 'username',
+      type: 'text',
+      label: '아이디',
+      value: formState.username,
+      placeholder: '아이디를 입력해주세요',
+    },
+    {
+      id: 'password',
+      type: 'password',
+      label: '비밀번호',
+      value: formState.password,
+      placeholder: '비밀번호를 입력해주세요',
+    },
+  ];
 
   //
-  // hook
+  // authStore
   //
-  const navigate = useNavigate();
+  const setLoginState = useAuthStore(state => state.login.action.setLoginState);
 
-  //
-  // callback
-  //
-  const onChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+  function  onChange(e: ChangeEvent<HTMLInputElement>) {
     const {
       id,
       value,
     } = e.target;
 
-    switch(id) {
-      case 'email':
-        setEmail(value);
-        break;
-      case 'password':
-        setPassword(value);
-        break;
+    setFormState(state => ({
+      ...state,
+      [id]: value,
+    }));
+  }
+
+  async function login(e?: FormEvent) {
+    e?.preventDefault();
+
+    const payload: TLoginPayload = formState;
+
+    try {
+      const response = await ApiManager
+        .auth
+        .login(payload);
+
+      const data = response.data;
+
+      console.log('login() 성공 - data: ', data);
+
+      setLoginState(createSuccessApiState(data));
+    } catch(error) {
+      // TODO: Sonar 컴포넌트로 메시지 렌더링하기
+      console.error(error);
     }
-  }, []);
-
-  const login = useCallback(() => {
-    console.group('login()');
-    console.log('email: ', email);
-    console.log('password: ', password);
-    console.groupEnd();
-
-    navigate(routePathFactory.math.getTextbookPath());
-  }, [email, password, navigate]);
-
-  //
-  // cache
-  //
-  const formTemplates = useMemo(() => [
-    {
-      id: 'email',
-      label: '아이디',
-      value: email,
-      placeholder: '아이디를 입력해주세요',
-    },
-    {
-      id: 'password',
-      label: '비밀번호',
-      value: password,
-      placeholder: '비밀번호를 입력해주세요',
-    },
-  ], [email, password]);
+  }
 
   return (
     <div className="LoginPage">
@@ -103,10 +118,13 @@ function LoginPage() {
         </div>
 
         {/* formBody */}
-        <div className="formBody">
+        <form 
+          className="formBody"
+          onSubmit={login}>
           {formTemplates.map(template => {
             const {
               id,
+              type,
               label,
               value,
               placeholder,
@@ -126,6 +144,7 @@ function LoginPage() {
                 <Input
                   className="input"
                   id={id}
+                  type={type}
                   value={value}
                   placeholder={placeholder}
                   autoComplete="off"
@@ -136,10 +155,10 @@ function LoginPage() {
 
           <Button
             className="loginButton"
-            onClick={login}>
+            type="submit">
             로그인
           </Button>
-        </div>
+        </form>
 
         {/* formFooter */}
         <div className="formFooter">
