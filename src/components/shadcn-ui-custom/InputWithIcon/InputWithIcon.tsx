c58ba4bfa7d4e96812b63aff7e24399fc8322d5c
Miles <yw@deepnatural.ai>
2024-07-31 18:37:13 +0900

    Create InputWithIcon

---
+++ src/components/shadcn-ui-custom/InputWithIcon/InputWithIcon.tsx
@@ -0,0 +1,55 @@
+import {
+  forwardRef,
+  memo,
+  InputHTMLAttributes,
+} from "react";
+import { cn } from '@/lib/shadcn-ui-utils';
+import { IconType } from 'react-icons';
+
+export interface InputWithIconProps
+  extends InputHTMLAttributes<HTMLInputElement> {
+  StartIcon?: IconType
+  EndIcon?: IconType
+}
+
+const _InputWithIcon = forwardRef<HTMLInputElement, InputWithIconProps>(({ 
+  className,
+  type,
+  StartIcon,
+  EndIcon,
+  ...props 
+}, ref) => {
+  return (
+    <div className="w-full relative">
+      <input
+        type={type}
+        className={cn(
+          "peer flex h-10 w-full rounded-md border border-input bg-background py-2 px-4 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50",
+          StartIcon ? "pl-10" : "",
+          EndIcon ? "pr-10" : "",
+          className
+        )}
+        ref={ref}
+        {...props}/>
+
+      {StartIcon && (
+        <StartIcon 
+          className={cn(
+            'absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-500 peer-focus:text-gray-900',
+            props.disabled ? 'cursor-not-allowed opacity-50' : ''
+          )} />
+      )}
+
+      {EndIcon && (
+        <EndIcon 
+          className={cn(
+            "absolute right-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-500 peer-focus:text-gray-900",
+            props.disabled ? 'cursor-not-allowed opacity-50' : ''
+          )} />
+      )}
+    </div>
+  );
+});
+_InputWithIcon.displayName = "InputWithIcon";
+
+export const InputWithIcon = memo(_InputWithIcon) as typeof _InputWithIcon;
