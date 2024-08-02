080c84c79b31e35ffbee8856344d30da54b8ebf3
Miles <yw@deepnatural.ai>
2024-08-02 15:48:50 +0900

    Edit Checkbox component

--- src/components/shadcn-ui/ui/checkbox.tsx
+++ src/components/shadcn-ui/ui/checkbox.tsx
@@ -1,28 +1,39 @@
 import * as React from "react"
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox"
 import { Check } from "lucide-react"
+import { RxDividerHorizontal } from 'react-icons/rx';
 
 import { cn } from "src/lib/shadcn-ui-utils"
+
+const CHECKBOX_INDETERMINATE = 'indeterminate';
 
 const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
->(({ className, ...props }, ref) => (
+>(({ className, checked, ...props }, ref) => (
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
       "peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
       className
     )}
+    checked={checked}
     {...props}
   >
     <CheckboxPrimitive.Indicator
-      className={cn("flex items-center justify-center text-current")}
+      className={cn(
+        "flex items-center justify-center text-current",
+        'w-full h-full'
+      )}
     >
-      <Check className="h-4 w-4" />
+      {checked === CHECKBOX_INDETERMINATE && <RxDividerHorizontal className="w-4 h-4" strokeWidth={1.5} />}
+      {checked === true && <Check className="w-4 h-4" strokeWidth={3.5} />}
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
 ))
 Checkbox.displayName = CheckboxPrimitive.Root.displayName
 
-export { Checkbox }
+export { 
+  Checkbox,
+  CHECKBOX_INDETERMINATE,
+}
