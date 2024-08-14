998fa099455d38ac8b5b9cebaeec71c79ed01d80
Miles <yw@deepnatural.ai>
2024-08-14 12:40:36 +0900

    Add wildcard to PathMatche hooks

--- src/layouts/AuthedSidebarLayout/hooks/useEnglishCMSPathMatch.ts
+++ src/layouts/AuthedSidebarLayout/hooks/useEnglishCMSPathMatch.ts
@@ -11,49 +11,49 @@ const useEnglishCMSPathMatch = () => {
   // 교과서
   const englishTextbookPathMatch = useMatch(routePathFactory
     .english
-    .getTextbookPath()
+    .getTextbookPath() + '/*'
   );
   const isEnglishTextbookPath = !!englishTextbookPathMatch;
 
   // 교과서 단원
   const englishChapterPathMatch = useMatch(routePathFactory
     .english
-    .getChapterPath()
+    .getChapterPath() + '/*'
   );
   const isEnglishChapterPath = !!englishChapterPathMatch;
 
   // 성취기준
   const englishAchievementPathMatch = useMatch(routePathFactory
     .english
-    .getAchievementPath()
+    .getAchievementPath() + '/*'
   );
   const isEnglishAchievementPath = !!englishAchievementPathMatch;
 
   // 지식개념
   const englishKnowledgeConceptPathMatch = useMatch(routePathFactory
     .english
-    .getKnowledgeConceptPath()
+    .getKnowledgeConceptPath() + '/*'
   );
   const isEnglishKnowledgeConceptPath = !!englishKnowledgeConceptPathMatch;
 
   // 시리즈-출처
   const englishSeriesSourcePathMatch = useMatch(routePathFactory
     .english
-    .getSeriesSourcePath()
+    .getSeriesSourcePath() + '/*'
   );
   const isEnglishSeriesSourcePath = !!englishSeriesSourcePathMatch;
 
   // 지문
   const englishInstructionPathMatch = useMatch(routePathFactory
     .english
-    .getInstructionPath()
+    .getInstructionPath() + '/*'
   );
   const isEnglishInstructionPath = !!englishInstructionPathMatch;
 
   // 문항
   const englishQuestionPathMatch = useMatch(routePathFactory
     .english
-    .getQuestionPath()
+    .getQuestionPath() + '/*'
   );
   const isEnglishQuestionPath = !!englishQuestionPathMatch;
 
