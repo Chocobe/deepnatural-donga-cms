3c9a0f844588fa22d3288e00a35e010e994d4348
Miles <yw@deepnatural.ai>
2024-08-22 11:04:29 +0900

    Remove TablePagination's "rows per page" element

--- src/components/shadcn-ui-custom/TablePagination/TablePagination.tsx
+++ src/components/shadcn-ui-custom/TablePagination/TablePagination.tsx
@@ -1,11 +1,5 @@
 // ui
 import { 
-  Select, 
-  SelectTrigger,
-  SelectContent,
-  SelectItem,
-} from '@/components/shadcn-ui/ui/select';
-import { 
   Button,
 } from '@/components/shadcn-ui/ui/button';
 // icon
@@ -16,16 +10,9 @@ import {
 // style
 import './TablePagination.css';
 
-const tableRowsPerPageItems = Array.from(
-  { length: 4 },
-  (_, i) => String((i + 1) * 50)
-);
-
 type TTablePaginationProps = {
   currentPage: number | string;
   lastPage: number | string;
-  rowsPerPage: number | string;
-  onChangeRowsPerPage: (rowsPerPage: number | string) => void;
   goToFirst: () => void;
   goToNext: () => void;
   goToPrevious: () => void;
@@ -36,8 +23,6 @@ function TablePagination(props: TTablePaginationProps) {
   const {
     currentPage,
     lastPage,
-    rowsPerPage,
-    onChangeRowsPerPage,
     goToFirst,
     goToPrevious,
     goToNext,
@@ -46,31 +31,6 @@ function TablePagination(props: TTablePaginationProps) {
 
   return (
     <div className="TablePagination">
-      <div className="TablePagination-rowsPerPageWrapper">
-        <div className="label">
-          Rows per page
-        </div>
-
-        <Select
-          value={String(rowsPerPage)}
-          onValueChange={onChangeRowsPerPage}>
-          <SelectTrigger className="trigger">
-            {rowsPerPage}개씩 보기
-          </SelectTrigger>
-
-          <SelectContent>
-            {tableRowsPerPageItems.map(value => (
-              <SelectItem
-                key={value}
-                className="item"
-                value={value}>
-                {value}
-              </SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
-      </div>
-
       <div className="TablePagination-paginationWrapper">
         <div className="indicator">
           Page {currentPage} of {lastPage}
