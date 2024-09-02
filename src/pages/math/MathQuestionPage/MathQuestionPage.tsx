adc7925fed10d7c37bd0a7bab63b1edc1328d05f
Miles <yw@deepnatural.ai>
2024-09-02 13:18:22 +0900

    Create MathQuestionHeader UI

--- src/pages/math/MathQuestionPage/MathQuestionPage.tsx
+++ src/pages/math/MathQuestionPage/MathQuestionPage.tsx
@@ -1,18 +1,28 @@
-// FIXME: mockup page
-import MockupPage from '@/components/pages/MockupPage/MockupPage';
+// ui
+import MathQuestionHeader from '@/components/pages/math/MathQuestionPage/MathQuestionHeader/MathQuestionHeader';
+// style
+import './MathQuestionPage.css';
 
-// react
-import {
-  memo,
-} from 'react';
-
-function _MathQuestionPage() {
+function MathQuestionPage() {
   return (
-    <MockupPage 
-      // isTestOverflow
-      mockupName="MathQuestion Page mockup" />
+    <div className="MathQuestionPage">
+      <div className="MathQuestionPage-header">
+        <MathQuestionHeader />
+      </div>
+
+      <div className="MathQuestionPage-actions">
+        Actions
+      </div>
+
+      <div className="MathQuestionPage-table">
+        Table
+      </div>
+
+      <div className="MathQuestionPage-footer">
+        Footer
+      </div>
+    </div>
   );
 }
 
-const MathQuestionPage = memo(_MathQuestionPage);
 export default MathQuestionPage;
