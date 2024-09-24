ce62e02ed595b644ba893bd169107a0ee0b75fc2
Miles <yw@deepnatural.ai>
2024-09-24 10:09:06 +0900

    Add mathPix env vars

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,8 @@
 interface ImportMetaEnv {
   readonly VITE_API_BASE_URL: string;
   readonly VITE_CMS_API_PATH: string;
+  readonly VITE_AWS_S3_API_BASE_URL: string;
+  readonly VITE_MATH_PIX_API_BASE_URL: string;
 }
 
 interface ImportMeta {
