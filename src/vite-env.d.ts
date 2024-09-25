f77795b786b5db4cff2422d2605b7301a7336c93
Miles <yw@deepnatural.ai>
2024-09-25 11:59:23 +0900

    Add VITE_DEVELOPMENT_API_BASE_URL env var

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,10 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_API_BASE_URL: string;
+  readonly VITE_PRODUCTION_API_BASE_URL: string;
+  readonly VITE_DEVELOPMENT_API_BASE_URL: string;
   readonly VITE_CMS_API_PATH: string;
+
   readonly VITE_AWS_S3_API_BASE_URL: string;
   readonly VITE_MATH_PIX_API_BASE_URL: string;
 }
